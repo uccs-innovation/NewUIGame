ac2df1795845f26fd52364265de42ef3f151f588
Chad18Z <credeurchad@gmail.com>
2019-09-27 12:13:19 -0600

    Level select scene added

---
+++ UI-Game/Assets/Scripts/Singleton/SingletonBehavior.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class SingletonBehavior<T> : MonoBehaviour where T : Component
+{
+    protected static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<T>();
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    obj.name = typeof(T).Name;
+                    instance = obj.AddComponent<T>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+            DontDestroyOnLoad(this.gameObject);
+            Init();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    protected virtual void Init()
+    {
+    }
+}
