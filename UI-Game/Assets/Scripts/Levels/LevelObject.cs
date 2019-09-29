d8f4f9deeb228684e626efcde95769ae226a8b0f
Chad18Z <credeurchad@gmail.com>
2019-09-28 21:00:15 -0600

    Level select UI now uses a grid layout group

---
+++ UI-Game/Assets/Scripts/Levels/LevelObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class LevelObject : ScriptableObject
+{
+    public string levelName = "Name of Level";
+
+    // Icon sprite for the level
+    public Sprite sprite;
+
+    // Here we can include some spawner parameters to control the speed/difficulty of the level
+
+    // The scene name of the level
+    public string sceneName = "Scene name";
+
+    // Description text for the level
+    public string descrText = "A sentence or two that describes this level";
+
+}
