da0e203678c821480e56e92a622589640610da9d
Chad18Z <credeurchad@gmail.com>
2019-09-28 23:17:36 -0600

    Dynamic buttons in level selector

--- UI-Game/Assets/Scripts/Managers/LevelManager.cs
+++ UI-Game/Assets/Scripts/Managers/LevelManager.cs
@@ -3,11 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
-    public void OnLevelOneSelected()
-    {       
-        SceneManager.LoadScene("MainScene");
+    [SerializeField]
+    GameObject placeholder;
+
+    [SerializeField]
+    GameObject portalButton;
+
+    [SerializeField]
+    GameObject portalPanel;
+
+    List<ScriptableObject> portals;
+
+    private void Start()
+    {
+        GameObject pbutton = Instantiate(portalButton, portalPanel.transform);
+        pbutton.name = "lvl_1";
+        pbutton.GetComponent<Button>().onClick.AddListener(OnLevelSelected);
+
+        GameObject button = Instantiate(placeholder, portalPanel.transform);
+        button.name = "lvl_2";
+        button.GetComponent<Button>().onClick.AddListener(OnLevelSelected);
+    }
+    public void OnLevelSelected()
+    {
+        // Get a reference to the currently selected button
+        GameObject callingButton = EventSystem.current.currentSelectedGameObject;
+
+        string sceneToLoad = GetScene(callingButton);
+
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    string GetScene(GameObject callingObject)
+    {
+        return ("MainScene");
     }
 }
