7819c8814c315b00ed771567c127a6a76ebbc60a
Chad18Z <credeurchad@gmail.com>
2019-09-30 22:03:02 -0600

    Credits and Instructions screens updated

--- UI-Game/Assets/Scripts/Menus/StartMenu.cs
+++ UI-Game/Assets/Scripts/Menus/StartMenu.cs
@@ -5,16 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnInstructionsButtonPressed()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("Instructions");
     }
 
     public void OnPlayButtonClicked()
--- UI-Game/Assets/Scripts/Scenes/InstructionsScene.cs
+++ UI-Game/Assets/Scripts/Scenes/InstructionsScene.cs
@@ -1,26 +1,52 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
+
 using System.Collections.Generic;
+
 using UnityEngine;
+
 using UnityEngine.SceneManagement;
+
 using UnityEngine.Events;
 
+
+
 public class ReturnEvent : UnityEvent
+
 {
+
+
 
 }
 
 
+
+
+
 public class InstructionsScreen : MonoBehaviour
+
 {
+
     ReturnEvent returnEvent = new ReturnEvent();
 
+
+
     public void HandleReturn()
+
     {
+
         returnEvent.Invoke();
+
     }
 
+
+
     public void AddHandler(UnityAction handler)
+
     {
+
         returnEvent.AddListener(handler);
+
     }
+
 }
